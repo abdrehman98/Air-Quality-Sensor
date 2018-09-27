3f05525003687a994c88c7d86862a215d799310c
Unknown <unknown@example.com>
2018-09-27 06:22:57 -0700

    UPDATED data till Evening

--- PYTHON CODE/read-save-serial-data.py
+++ PYTHON CODE/read-save-serial-data.py
@@ -1,13 +1,25 @@
 import serial
 import sys
 import time
-print('Please Enter serial port at which arduino is attached:')
+
+
+print('************************************************************')
+print('*  A pthon code written for Air Quality Sensor Calibration *')
+print('*        It will Read data from USB port and               *')
+print('*      Display on Screen and Same on File as named         *')
+print('*           exactly same as port name                      *')
+print('************************************************************')
+
+print('ENTER Serial-Porrt of Attched ARDUINO:')
 serial_port = input()
-f = open(serial_port + ".csv", "a+")
+
+f = open('..\\DATA\\SEP-27\\' + serial_port + ".csv", "a+")
+
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = serial_port
 ser.open()
+
 while True:
     data = ser.readline()
     data = data.decode('ascii')

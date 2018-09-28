997bb29bfceab90317f712d99691516bbe1b1b32
Zeeshan-Developer <bscs15078@itu.edu.pk>
2018-09-28 04:34:02 -0700

    Data added for sensor DSM501A Analysis 1

--- PYTHON CODE/read-save-serial-data.py
+++ PYTHON CODE/read-save-serial-data.py
@@ -13,29 +13,17 @@ print('************************************************************')
 print('ENTER Serial-Porrt of Attched ARDUINO:')
 serial_port = input()
 
+f = open('..\\DATA\\SEP-27\\' + serial_port + ".csv", "a+")
+
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = serial_port
 ser.open()
-n = 0
-list = []
+
 while True:
     data = ser.readline()
     data = data.decode('ascii')
     data = data.replace('\r\n','')
-    data = data + "," + str(time.time())
-    print(data, "---->", n)
-    list.append(data)
-    n = n + 1
-    if n == 1000:
-        n = 0
-        print('***** Opening File   *****')
-        f = open('..\\DATA\\SEP-27\\' + serial_port + ".csv", "a+")
-        print('***** Structuring Data ***')
-        list = '\n'.join(list) + '\n'
-        print('***** Writting data  *****')
-        f.write(list)
-        print("***** Closing File   *****")
-        f.close
-        list = []
-        print("***** Happy Restart  *****")
+    data = data + "," + str(time.time()) + "\n"
+    print(data)
+    f.write(data)

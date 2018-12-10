7b6e45ea18c746417869ee03135c74f96a730e76
Makeistan PC <bscs15078@itu.edu.pk>
2018-12-10 16:53:34 +0500

    PMS3003: allowed error added

--- SENSOR-CALIBRATION/ERROR-CHECK-CODE/error_check.m
+++ SENSOR-CALIBRATION/ERROR-CHECK-CODE/error_check.m
@@ -27,6 +27,7 @@ start_ts = max([min(ts1) min(ts2) min(ts3)]);
 end_ts = min([max(ts1) max(ts2) max(ts3)]);
 uts = start_ts:minutes(60):end_ts;
 
+error_bound = zeros(size(uts));
 to_pm25_sp_1 = zeros(size(uts));
 to_pm25_sp_2 = zeros(size(uts));
 to_pm25_sp_3 = zeros(size(uts));
@@ -43,16 +44,24 @@ for index = 1:length(uts) - 1
 
     logic_indicies = ts3 >= lower & ts3 < upper;
     to_pm25_sp_3(index) = mean(pm25_sp_3(logic_indicies));
+    
+    max_val = max([to_pm25_sp_1(index) to_pm25_sp_2(index) to_pm25_sp_3(index)]);
+    if max_val <= 100
+        error_bound(index) = 10;
+    elseif max_val > 100 && max_val <= 500
+        error_bound(index) = (max_val / 100) * 10;
+    else
+        error_bound(index) = (max_val / 100) * 20;
+    end  
 end
 
 % plot(ts1, pm25_sp_1, 'r', ts2, pm25_sp_2, 'b', ts3, pm25_sp_3, 'g')
 % plot(uts, to_pm25_sp_1, 'r' , uts, to_pm25_sp_2, 'b', uts, to_pm25_sp_3, 'g');
-plot(uts, abs(to_pm25_sp_1 - to_pm25_sp_2), 'r', uts, abs(to_pm25_sp_2 - to_pm25_sp_3), 'b', uts, abs(to_pm25_sp_1 - to_pm25_sp_3), 'g');
-
-title('PM2.5 ug/m3 two day data: Indoor testing (Absolute error vector: sampled at 1 hour)');
+plot(uts, error_bound, 'black', uts, abs(to_pm25_sp_1 - to_pm25_sp_2), 'r', uts, abs(to_pm25_sp_2 - to_pm25_sp_3), 'b', uts, abs(to_pm25_sp_1 - to_pm25_sp_3), 'g');
+title('PM2.5 ug/m3 two day data: Indoor testing (Absolute error vector: sampled at 60 minute)');
 xlabel('Time: 2 Days');
 ylabel('PM2.5: measured in ug/m3');
 % legend({'S1', 'S2', 'S3'},'Location','northwest')
-legend({'|S1 - s2|', '|S2 - s3|', '|S3 - s1|'}, 'Location', 'northwest');
+legend({'allowed error', '|S1 - s2|', '|S2 - s3|', '|S3 - s1|'}, 'Location', 'northwest');
 
 clear

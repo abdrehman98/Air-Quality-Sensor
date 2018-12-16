3042fb760b5058ad30e1178fc0478cbdf0d91c52
Makeistan PC <bscs15078@itu.edu.pk>
2018-12-16 20:24:54 +0500

    More generalization

--- SENSOR-CALIBRATION/ERROR-CHECK-CODE/error_check.m
+++ SENSOR-CALIBRATION/ERROR-CHECK-CODE/error_check.m
@@ -1,9 +1,20 @@
 TIME_STAMP_INDEX = 7;
 GUASS_FILTER_WINDOW_SIZE = 10;
-MIN_STEP = 1
-MAX_STEP = 60
-%guass_filter = gausswin(GUASS_FILTER_WINDOW_SIZE) ./ GUASS_FILTER_WINDOW_SIZE;
-guass_filter  = (1/GUASS_FILTER_WINDOW_SIZE) * ones(1,GUASS_FILTER_WINDOW_SIZE);
+MIN_STEP = 1;
+MAX_STEP = 5;
+
+% DATA_VAL = 2;
+% LABLE_Y = 'PM2.5: measured in ug/m3';
+% DATA_VAL = 3;
+% LABLE_Y = 'PM10.0: measured in ug/m3';
+% DATA_VAL = 1;
+% LABLE_Y = 'PM1.0: measured in ug/m3';
+% LABLE_Y = 'SP';
+LABLE_Y = 'Enviroment';
+
+% ------------------------------------- To apply filter on data
+% created_filter = gausswin(GUASS_FILTER_WINDOW_SIZE) ./ GUASS_FILTER_WINDOW_SIZE;
+% created_filter  = (1/GUASS_FILTER_WINDOW_SIZE) * ones(1,GUASS_FILTER_WINDOW_SIZE);
 
 data_s1 = csvread('../EXTRECTED-DATA/DEC-2/s1.csv');
 data_s2 = csvread('../EXTRECTED-DATA/DEC-2/s2.csv');
@@ -24,34 +35,42 @@ disp('Done with timestamps')
 %---------------------------------------------------------
 
 for range = MIN_STEP:1:MAX_STEP
-    pm25_sp_1 = data_s1(:, 2);
-    pm25_sp_2 = data_s2(:, 2);
-    pm25_sp_3 = data_s3(:, 2);
+
+    % pm1 = data_s1(:, 1);
+    % pm2 = data_s2(:, 2);
+    % pm3 = data_s3(:, 3);
+    pm1 = data_s1(:, 4);
+    pm2 = data_s2(:, 5);
+    pm3 = data_s3(:, 6);
+    % pm1 = data_s1(:, DATA_VAL);
+    % pm2 = data_s2(:, DATA_VAL);
+    % pm3 = data_s3(:, DATA_VAL);
     disp('Extracting data:');
-
+    
     uts = start_ts:minutes(range):end_ts;
-    pm25_sp_1 = samplize(ts1, pm25_sp_1, uts);
-    pm25_sp_2 = samplize(ts2, pm25_sp_2, uts);
-    pm25_sp_3 = samplize(ts3, pm25_sp_3, uts);
+    pm1 = samplize(ts1, pm1, uts);
+    pm2 = samplize(ts2, pm2, uts);
+    pm3 = samplize(ts3, pm3, uts);
     disp('Sapmplizing data');
-    % pm25_sp_1 = filter(guass_filter, 1, pm25_sp_1);
-    % pm25_sp_2 = filter(guass_filter, 1, pm25_sp_2);
-    % pm25_sp_3 = filter(guass_filter, 1, pm25_sp_3);
 
-    plot(uts, pm25_sp_1, 'r', uts, pm25_sp_2, 'b', uts, pm25_sp_3, 'g')
-    title('PM2.5 ug/m3 two day data: Indoor testing (Absolute error vector: sampled at 60 minute)');
+    % pm25_sp_1 = filter(created_filter, 1, pm25_sp_1);
+    % pm25_sp_2 = filter(created_filter, 1, pm25_sp_2);
+    % pm25_sp_3 = filter(created_filter, 1, pm25_sp_3);
+    % disp('Apply filter');
+
+    plot(uts, pm1, 'r', uts, pm2, 'b', uts, pm3, 'g');
+    title(strcat('Two day data: Indoor testing (Absolute error vector: sampled at (', num2str(range), ' minute)'));
     xlabel('Time: 2 Days');
-    ylabel('PM2.5: measured in ug/m3');
+    ylabel(LABLE_Y);
     legend({'S1', 'S2', 'S3'},'Location','northwest')
-
-
+    
     %--------------------------------------------------------------------
     %---------------------------- Uncomment to plot errors
-    % error_bound = max_error([pm25_sp_1; pm25_sp_2; pm25_sp_3]);
-    % plot(uts, error_bound, 'black', uts, abs(to_pm25_sp_1 - to_pm25_sp_2), 'r', uts, abs(to_pm25_sp_2 - to_pm25_sp_3), 'b', uts, abs(to_pm25_sp_1 - to_pm25_sp_3), 'g');
-    % title('PM2.5 Error ug/m3 two day data: Indoor testing (Absolute error vector: sampled at 60 minute)');
+    % error_bound = max_error([pm1; pm2; pm3]);
+    % plot(uts, error_bound, 'black', uts, abs(pm1 - pm2), 'r', uts, abs(pm2 - pm3), 'b', uts, abs(pm1 - pm3), 'g');
+    % title('Error ug/m3 two day data: Indoor testing (Absolute error vector: sampled at 60 minute)');
     % xlabel('Time: 2 Days');
-    % ylabel('PM2.5: measured in ug/m3');
+    % ylabel(LABLE_Y);
     % legend({'allowed error', '|S1 - s2|', '|S2 - s3|', '|S3 - s1|'}, 'Location', 'northwest');
 
     disp('Move to next plot --')

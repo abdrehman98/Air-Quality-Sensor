1a2a807c2aef5117c59f0c68355fe7c118716ce0
Zeeshan Iqbal Makeistan-Laptop <bscs15078@itu.edu.pk>
2019-01-09 22:43:57 +0500

    Results added

--- SENSOR-CALIBRATION/ERROR-CHECK-CODE/av_vs_pms.py
+++ SENSOR-CALIBRATION/ERROR-CHECK-CODE/av_vs_pms.py
@@ -1,5 +1,5 @@
 import indices as I
-from datetime import datetime
+from datetime import datetime, timedelta
 import consts as C
 from matplotlib import pyplot as plotter
 import GENERAL
@@ -9,12 +9,11 @@ def load_air_visual_data(file_path):
 
     # open file for reading data
     data = open(file_path).readlines()
-
     # Remove Tags
     data.remove(data[0])
-
     print("Loading Air visuals data . . .")
     structured_data = []
+
     for line in data:
         # Remove Separator from the end of line
         line = line.replace(line[len(line) - 1], "")
@@ -24,7 +23,8 @@ def load_air_visual_data(file_path):
         if len(line) == 14:
             eline = [None] * I.NUMBER_OF_ENCODED_COLUMNS
 
-            eline[I.E_TIMESTAMP] = datetime.fromtimestamp(float(line[I.TIMESTAMP]))
+            eline[I.E_TIMESTAMP] = datetime.fromtimestamp(
+                float(line[I.TIMESTAMP])) - timedelta(hours=10)
             eline[I.E_AQI] = float(line[I.AQI_US])
             eline[I.E_PM25] = float(line[I.PM2_5])
             eline[I.E_PM10] = float(line[I.PM10])
@@ -32,15 +32,48 @@ def load_air_visual_data(file_path):
             eline[I.E_HUMIDITY] = float(line[I.HUMIDITY_RH])
             structured_data.append(eline)
 
-    print("Done loading . . .")
+    print("Done loading!!")
+    return structured_data
+
+
+def load_pms_data(file_path):
+
+    # open file for reading data
+    data = open(file_path).readlines()
+    print("Loading PMS3003 data . . .")
+    structured_data = []
+
+    for line in data:
+        # Remove Separator from the end of line
+        line = line.replace(line[len(line) - 1], "")
+        # Split entries on token
+        line = line.split(C.PMS_FILE_SEPARATOR)
+
+        if len(line) == I.PMS_NUMBER_DATA_COLUMNS:
+            for i in range(len(line)):
+                line[i] = float(line[i])
+            line[I.PMS_TIMESTAMP] = datetime.utcfromtimestamp(line[I.PMS_TIMESTAMP])
+            structured_data.append(line)
+
+    print("Done loading !!")
     return structured_data
 
 
 def main():
     paqi_data = load_air_visual_data(C.PAQI_DATA_FILE_PATH)
-    # pms_data = load_data(C.PMS_DATA_FILE_PATH, C.PMS_FILE_SEPARATOR, 7)
+    pms_data = load_pms_data(C.PMS_DATA_FILE_PATH)
     plotter.plot(GENERAL.get_column(I.E_TIMESTAMP, paqi_data),
-                 GENERAL.get_column(I.E_PM25, paqi_data))
+                 GENERAL.get_column(I.E_PM25, paqi_data),
+                 label="Air Visual: PM2.5")
+
+    plotter.plot(GENERAL.get_column(I.PMS_TIMESTAMP, pms_data),
+                 GENERAL.get_column(I.PMS_AE_2_5, pms_data),
+                 label="PMS3003: PM2.5 AE format")
+    plotter.grid()
+    plotter.xlabel("Timestamp")
+    plotter.ylabel("PM2.5 concentration: ug/m^3")
+    plotter.title("PMS3003 VS Air visual (pro node)")
+    plotter.legend()
     plotter.show()
 
 

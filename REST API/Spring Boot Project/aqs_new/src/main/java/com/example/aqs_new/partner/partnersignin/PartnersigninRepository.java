53b0af3dc2c8b6b0b790e42a49255eacd589b629
Muhammad Farooq <bscs15006@itu.edu.pk>
2019-02-01 18:04:28 +0500

    Version 0.0 Build generated

--- REST API/Spring Boot Project/aqs_new/src/main/java/com/example/aqs_new/partner/partnersignin/PartnersigninRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aqs_new.partner.partnersignin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartnersigninRepository extends JpaRepository<Partnersignin,Long> {
-
-}

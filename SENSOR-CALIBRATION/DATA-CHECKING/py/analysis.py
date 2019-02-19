d56d6ed7f0fc1606880470c61c4d78d55677f354
Zeeshan Iqbal <bscs15078@itu.edu.pk>
2019-02-19 14:22:57 +0500

    Compiled error results

--- SENSOR-CALIBRATION/DATA-CHECKING/py/analysis.py
+++ SENSOR-CALIBRATION/DATA-CHECKING/py/analysis.py
@@ -1,6 +1,10 @@
-from matplotlib import pyplot as pyplot
+from collections import Counter
 
+from matplotlib import pyplot
+from matplotlib import dates
+from matplotlib.collections import PatchCollection
 import consts as C
+import statistics
 from ind import *
 from load_data import Load
 
@@ -14,28 +18,41 @@ Fine sampling was (0.3 - 2) s based upon air quality gradient
 ______________________________________________________________
 For all the Measurements "Air Visual" is used as ground truth
 """
-TITLE = "PM2.5: PMS3003 and Air visual"
 
 
 def fit(x):
     x = float(x)
 
     # Constants
-    p1 = 9.068e-07
-    p2 = -0.0005488
-    p3 = 0.7456
-    p4 = -0.8601
+    c1 = 9.068e-07
+    c2 = -0.0005488
+    c3 = 0.7456
+    c4 = -0.8601
 
     # Degrees of fit
-    y3 = p1 * (x ** 3)
-    y2 = p2 * (x ** 2)
-    y1 = p3 * x
-    y0 = p4
+    y3 = c1 * pow(x, 3)
+    y2 = c2 * pow(x, 2)
+    y1 = c3 * x
+    y0 = c4
 
     # Final equation
     y = y3 + y2 + y1 + y0
 
     return y
+
+
+def hist(v, r):
+    y = [0] * (len(r) - 1)
+    for vn in v:
+        for i in range(len(y)):
+            if r[i] <= vn <= r[i + 1]:
+                y[i] += 1
+
+    x = [None] * len(y)
+    for i in range(len(x)):
+        x[i] = "[ " + str(r[i]) + " - " + str(r[i + 1]) + " ]"
+
+    return x, y
 
 
 def main():
@@ -51,25 +68,60 @@ def main():
     y = data1[AV.PM25]
     x = data2[PMS.AE25]
 
+    ###
+    # Computation of vectors
+    ###
+    # Compute: error
     x1 = [fit(k) for k in x]
+    dxy = [abs(k1 - k2) for k1, k2 in zip(x1, y)]
+    pxy = [round(dxy[i] / y[i], 2) * 100 for i in range(len(uts))]
+    # compute: fit
+    x0 = list(range(900))
+    y0 = [fit(k) for k in x0]
 
-    ###############################################
-    ###############################################
-    ########### Custom plotting starts here #######
-    ###############################################
-    ###############################################
+    hx, hy = hist(pxy, list(range(0, 14, 3)))
+    hy = [h / sum(hy) * 100 for h in hy]
+    ###
+    # GRAPH PLOTTING: starts here
+    ###
 
-    pyplot.title(TITLE)
-    pyplot.plot(uts, x)
-    pyplot.plot(uts, x1)
-    pyplot.plot(uts, y)
-    pyplot.show()
+    title = "PERCENTAGE-ERROR 2"
+    pyplot.figure(figsize=(16 * 0.8, 7 * 0.8))
+    pyplot.title(title)
+    pyplot.plot(uts, pxy, "m") # , label="PERCENTAGE ERROR")
+    pyplot.xlabel("Error occurred")
+    pyplot.xlim([min(uts), max(uts)])
+    # pyplot.ylim([0, 50])
+    pyplot.grid()
+    pyplot.legend()
+    pyplot.gcf().autofmt_xdate()
+    # pyplot.show()
+    pyplot.savefig(title + ".png", dpi=200)
 
+    """
+    # Print values:
+    print("Mean difference: ", statistics.mean(pxy), "%")
+    print("Median of difference: ", statistics.median(pxy), "%")
+    print("Mode of the difference: ", Counter(pxy).most_common(1), "%")
+    print("Standard deviation: ", statistics.pstdev(pxy), "%")
+    print("Variance: ", statistics.pvariance(pxy), "%")
+    """
 
 main()
 
 
 """
+COLOR CONVENTION
+AIR-VISUAL = BLUE
+PMS3003-AE = RED
+CALIBRATED-PMS3003-AE = GREEN
+
+
+"""
+
+
+"""
+    pyplot.ylabel("Time in (Month-Day Hour)     Year = 2018")
 # PLOT: data
 plotter.plot(uts, av_PM25, label="AIR-VISUAL Node pro data")
 plotter.plot(uts, pms_PM25, label="PMS3003 AE")
